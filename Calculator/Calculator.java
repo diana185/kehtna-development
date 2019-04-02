70433449a8a15ee386a17b52ac95ea38b294a5cc
diana185 <diana_saar@hotmail.com>
2019-04-02 13:04:25 +0300

    Create Calculator.java

---
+++ Calculator/Calculator.java
@@ -0,0 +1,22 @@
+public class Calculator {
+  public static void main(String[]args) {
+
+    System.out.println(String[]args);
+  }
+
+  private int sum(a, b) {
+    return a+b;
+  }
+
+  private int subtract(a, b) {
+    return a-b;
+  }
+
+  private float divide(a, b) {
+    return a/b;
+  }
+
+  private int multiply(a, b) {
+    return a*b;
+  }
+}
