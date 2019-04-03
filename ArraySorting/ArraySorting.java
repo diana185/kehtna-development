bc1b12b401c7683d8e221d67fe0b70c04be4ae80
diana185 <diana_saar@hotmail.com>
2019-04-03 10:35:06 +0300

    Update ArraySorting.java

--- ArraySorting/ArraySorting.java
+++ ArraySorting/ArraySorting.java
@@ -4,7 +4,6 @@ import java.util.Random;
 public class ArraySorting {
   public static void main(String[] args) {
     int size, min, max;
-    int[] randomArray = new int[size];
 
     if (args.length == 0) {
       Scanner scanner = new Scanner(System.in);
@@ -15,24 +14,65 @@ public class ArraySorting {
       System.out.print("Enter maximum number: ");
       max = Integer.parseInt(scanner.next());
     } else {
-      size = Integer.parseInt(args[0]);
-      min = Integer.parseInt(args[1]);
-      max = Integer.parseInt(args[2]);
+      if (isInteger(args[0]) && isInteger(args[1]) && isInteger(args[2])) {
+        size = Integer.parseInt(args[0]);
+        min = Integer.parseInt(args[1]);
+        max = Integer.parseInt(args[2]);
+      } else {
+        System.out.println("Entered data is incorrect!");
+        return;
+      }
     }
+
+    int[] randomArray = new int[size];
 
     for (int i = 0; i < size; i++) {
-      randomArray[i] = (rng(min, max));
+      randomArray[i] = (randInt(min, max));
     }
 
-    for (int i = 0; i < size; i++) {
-      System.out.println(randomArray[i]);
+    printArray(randomArray);
+    System.out.println("- random");
+
+    for (int i = 0; i < randomArray.length-1; i++) {
+      for (int j=i+1; j < randomArray.length; j++) {
+        if (randomArray[i] > randomArray[j]) {
+          swap(randomArray, i, j);
+        }
+      }
     }
 
+    printArray(randomArray);
+    System.out.println("- sorted");
   }
 
-  public static int rng(int min, int max) {
+  public static void printArray(int[] array) {
+    for (int i = 0; i < array.length; i++) {
+      System.out.print(array[i] + " ");
+    }
+  }
+
+  public static void swap(int[] array, int i, int j) {
+    int aux = array[i];
+    array[i] = array[j];
+    array[j] = aux;
+  }
+
+  public static int randInt(int min, int max) {
     Random random = new Random();
     int randomNumber = random.nextInt((max - min) + 1) + min;
     return randomNumber;
+  }
+
+  public static boolean isInteger(String str) {
+    try {
+      Integer.parseInt(str);
+      return true;
+    } catch (NumberFormatException ex) {
+      return false;
+    }
+  }
+
+  public static void incorrect() {
+    System.out.println("Entered data is incorrect!");
   }
 }
