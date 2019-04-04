5128a9c37d0fb79dbb6651984eb41de60a1b005e
diana185 <diana_saar@hotmail.com>
2019-04-04 14:27:58 +0300

    Update NavalBattle.java

--- NavalBattle/src/NavalBattle.java
+++ NavalBattle/src/NavalBattle.java
@@ -1,13 +1,12 @@
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Random;
 import java.util.Scanner;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class NavalBattle implements Runnable {
 
-  /**
-   *
-   */
-  private boolean gameRunning = false;
+  private boolean gameRunning;
 
   /**
    * Main method
@@ -29,7 +28,6 @@ public class NavalBattle implements Runnable {
           size = Integer.parseInt(args[0]);
         } catch (Exception e) {
           size = defaultSize;
-          System.out.println("Used default value.");
         }
         break;
       case 2:
@@ -37,40 +35,34 @@ public class NavalBattle implements Runnable {
           size = Integer.parseInt(args[0]);
         } catch (Exception e) {
           size = defaultSize;
-          System.out.println("Used default value.");
         }
         try {
           size = Integer.parseInt(args[1]);
         } catch (Exception e) {
           ships = defaultShips;
-          System.out.println("Used default value.");
         }
         break;
       default:
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter board size: ");
+        System.out.print("Enter board size (5 - 20, default 10):  ");
         try {
           size = Integer.parseInt(scanner.nextLine());
         } catch (Exception e) {
           size = defaultSize;
-          System.out.println("Used default value.");
         }
-        System.out.println("Enter the amount of ships: ");
+        System.out.print("Enter the amount of ships (3 - 1/3 of size, default 1/2 of size): ");
         try {
           ships = Integer.parseInt(scanner.nextLine());
         } catch (Exception e) {
           ships = defaultShips;
-          System.out.println("Used default value.");
         }
         break;
     }
     if (size > maxSize || size < minSize) {
       size = defaultSize;
-      System.out.println("Used default value.");
     }
     if (ships > maxShips || ships < minShips) {
       ships = defaultShips;
-      System.out.println("Used default value.");
     }
     NavalBattle app = new NavalBattle(size, ships);
     Thread thread = new Thread(app);
@@ -85,7 +77,7 @@ public class NavalBattle implements Runnable {
       //System.out.println("Running...");
       try {
         Thread.sleep(1000);
-        clearScreen();
+        //clearScreen();
       } catch(Exception e) {
         // Exception
       }
@@ -99,11 +91,13 @@ public class NavalBattle implements Runnable {
    */
   @SuppressWarnings("WeakerAccess")
   public NavalBattle(int size, int ships) {
+    gameRunning = true;
     int[] array = genArray(size, ships);
+    int[] playerArray = genArray(size, 0);
+    int[][] playerBoard = arrayToBoard(playerArray, size);
     int[][] board = arrayToBoard(array, size);
-    printBoard(board);
-    System.out.println(size);
-    System.out.println(ships);
+    printBoard(playerBoard);
+    saveBoard();
   }
 
   /**
@@ -120,12 +114,12 @@ public class NavalBattle implements Runnable {
     }
   }
 
-  /**
+  /*
    * Converts a 2D array to a 1D array
    * @param board 2D array - int[][]
    * @return 1D array - int[]
    */
-  private int[] boardToArray(int[][] board) {
+  /*private int[] boardToArray(int[][] board) {
     int[] array = new int[(board.length * board[0].length)];
     int k = 0;
     for(int i = 0; i < board.length; i++) {
@@ -135,7 +129,7 @@ public class NavalBattle implements Runnable {
       }
     }
     return array;
-  }
+  }*/
 
   /**
    * Generates an array
@@ -179,8 +173,20 @@ public class NavalBattle implements Runnable {
    * @param board - array to be printed - int[][]
    */
   private void printBoard(int[][] board) {
+    char[] colMarker = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".toCharArray();
+    String rowMarker;
+    System.out.println();
+    for (int i = 0; i < board[0].length; i++) {
+      System.out.print(colMarker[i] + " ");
+    }
     System.out.println();
     for (int i = 0; i < board.length; i++) {
+      if (i < 9) {
+        rowMarker = " " + (i+1) + "| ";
+      } else {
+        rowMarker = (i+1) + "| ";
+      }
+      System.out.print(rowMarker);
       for (int j = 0; j < board[0].length; j++) {
         System.out.print(board[i][j] + " ");
       }
@@ -188,24 +194,34 @@ public class NavalBattle implements Runnable {
     }
   }
 
-  /**
+  private void saveBoard() {
+    try {
+      FileWriter fileWriter = new FileWriter("progress.txt");
+      fileWriter.write("saved game");
+      fileWriter.close();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+
+  /*
    * Prints a 1D array
    * @param array array to be printed - itn[]
    */
-  private void printArray(int[] array) {
+  /*private void printArray(int[] array) {
     System.out.println();
       for (int value : array) System.out.print(value + " ");
     System.out.println();
-  }
+  }*/
 
-  /**
+  /*
    * Clears screen only in Windows Command Prompt
    */
-  private void clearScreen() {
+  /*private void clearScreen() {
     try {
       new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
     } catch(Exception e) {
         //
     }
-  }
+  }*/
 }
