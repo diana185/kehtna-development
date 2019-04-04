5c20edbd822ece27205aaa4af02229654db984a6
diana185 <diana_saar@hotmail.com>
2019-04-04 13:40:34 +0300

    Update NavalBattle.java

--- NavalBattle/src/NavalBattle.java
+++ NavalBattle/src/NavalBattle.java
@@ -4,6 +4,9 @@ import java.util.concurrent.ThreadLocalRandom;
 
 public class NavalBattle implements Runnable {
 
+  /**
+   *
+   */
   private boolean gameRunning = false;
 
   /**
@@ -13,21 +16,61 @@ public class NavalBattle implements Runnable {
   public static void main(String[] args) {
     int size = 5;
     int ships = ((size * size) / 3);
+    int minSize = 5;
+    int maxSize = 20;
+    int defaultSize = 10;
+    int minShips = 3;
+    int maxShips = ((size * size) / 3);
+    int defaultShips = ((size * size) / 2);
+
     switch(args.length) {
       case 1:
-        size = Integer.parseInt(args[0]);
+        try {
+          size = Integer.parseInt(args[0]);
+        } catch (Exception e) {
+          size = defaultSize;
+          System.out.println("Used default value.");
+        }
         break;
       case 2:
-        size = Integer.parseInt(args[0]);
-        ships = Integer.parseInt(args[1]);
+        try {
+          size = Integer.parseInt(args[0]);
+        } catch (Exception e) {
+          size = defaultSize;
+          System.out.println("Used default value.");
+        }
+        try {
+          size = Integer.parseInt(args[1]);
+        } catch (Exception e) {
+          ships = defaultShips;
+          System.out.println("Used default value.");
+        }
         break;
       default:
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter board size: ");
-        size = Integer.parseInt(scanner.nextLine());
+        try {
+          size = Integer.parseInt(scanner.nextLine());
+        } catch (Exception e) {
+          size = defaultSize;
+          System.out.println("Used default value.");
+        }
         System.out.println("Enter the amount of ships: ");
-        ships = Integer.parseInt(scanner.nextLine());
+        try {
+          ships = Integer.parseInt(scanner.nextLine());
+        } catch (Exception e) {
+          ships = defaultShips;
+          System.out.println("Used default value.");
+        }
         break;
+    }
+    if (size > maxSize || size < minSize) {
+      size = defaultSize;
+      System.out.println("Used default value.");
+    }
+    if (ships > maxShips || ships < minShips) {
+      ships = defaultShips;
+      System.out.println("Used default value.");
     }
     NavalBattle app = new NavalBattle(size, ships);
     Thread thread = new Thread(app);
@@ -59,12 +102,12 @@ public class NavalBattle implements Runnable {
     int[] array = genArray(size, ships);
     int[][] board = arrayToBoard(array, size);
     printBoard(board);
-    //System.out.println(size);
-    //System.out.println(ships);
+    System.out.println(size);
+    System.out.println(ships);
   }
 
   /**
-   * Array shuffler
+   * Shuffles an array
    * @param array Array to be shuffled - int[]
    */
   private void shuffleArray(int[] array) {
@@ -77,6 +120,11 @@ public class NavalBattle implements Runnable {
     }
   }
 
+  /**
+   * Converts a 2D array to a 1D array
+   * @param board 2D array - int[][]
+   * @return 1D array - int[]
+   */
   private int[] boardToArray(int[][] board) {
     int[] array = new int[(board.length * board[0].length)];
     int k = 0;
@@ -89,6 +137,12 @@ public class NavalBattle implements Runnable {
     return array;
   }
 
+  /**
+   * Generates an array
+   * @param size Size of the array - int
+   * @param ships Amount of ships - int
+   * @return returns array
+   */
   private int[] genArray(int size, int ships) {
     int[] array = new int[(size * size)];
     for (int a = 0; a < array.length; a++) {
@@ -102,6 +156,12 @@ public class NavalBattle implements Runnable {
     return array;
   }
 
+  /**
+   * Converts a 1D array to 2D array
+   * @param array 1D array - int[]
+   * @param size size of the 2D array
+   * @return returns 2D array - int[][]
+   */
   private int[][] arrayToBoard(int[] array, int size) {
     int[][] board = new int[size][size];
     int k = 0;
@@ -114,6 +174,10 @@ public class NavalBattle implements Runnable {
     return board;
   }
 
+  /**
+   * Prints a 2D array
+   * @param board - array to be printed - int[][]
+   */
   private void printBoard(int[][] board) {
     System.out.println();
     for (int i = 0; i < board.length; i++) {
@@ -124,12 +188,19 @@ public class NavalBattle implements Runnable {
     }
   }
 
+  /**
+   * Prints a 1D array
+   * @param array array to be printed - itn[]
+   */
   private void printArray(int[] array) {
     System.out.println();
       for (int value : array) System.out.print(value + " ");
     System.out.println();
   }
 
+  /**
+   * Clears screen only in Windows Command Prompt
+   */
   private void clearScreen() {
     try {
       new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
